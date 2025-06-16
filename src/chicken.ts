c0e0120a2179b186c39c265dd07b57f62e4f7418
Tariqul Islam <tariqul.isha@gmail.com>
2025-06-16 18:12:07 +0100

    initial commit for typescript boilerplate code

---
+++ src/chicken.ts
@@ -0,0 +1,8 @@
+export default class Chicken {
+    constructor() {
+
+    }
+    cluck = () => {
+        console.log('Cluck Chicken 34fsdfsd44');
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import Chicken from "./chicken";
+
+console.log('Hello wordl!');
+
+const chicken = new Chicken();
+chicken.cluck();
